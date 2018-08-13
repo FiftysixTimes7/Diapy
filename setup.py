dda1093d9b0c1e1e03fd6b5c5099331ffa0e8e10
FiftysixTimes7(PTJ) <pangtj26@163.com>
2018-08-13 14:32:31 +0800

    remove rst

--- setup.py
+++ setup.py
@@ -2,12 +2,19 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
+try:
+    with open('README.md') as f:
+        readme = f.read()
+except:
+    readme = '''A small diary manager based on python.
+Diapy uses cryptography.fernet to encrypt your top secret.'''
+
 setup(
     name='diapy',
     version='3.0.0',
     description='A small diary manager based on python.',
-    long_description='''A small diary manager based on python.
-Diapy uses cryptography.fernet to encrypt your top secret.''',
+    long_description=readme,
+    long_description_content_type='text/markdown',
     url='https://github.com/FiftysixTimes7/diapy',
     author='FiftysixTimes7(PTJ)',
     author_email='pangtj26@163.com',
